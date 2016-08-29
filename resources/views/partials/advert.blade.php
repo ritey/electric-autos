4f6546be56f6ca7645934d3442ccb6a8394e94a6
David Wright <dave@ritey.com>
2016-08-29 19:06:37 +0100

    Google Adsense

--- resources/views/partials/advert.blade.php
+++ resources/views/partials/advert.blade.php
@@ -1,1 +1,11 @@
 <h1>Advert</h1>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- electronic autos -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3396683885146934"
+     data-ad-slot="4795896302"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
