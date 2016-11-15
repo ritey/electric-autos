d0654f9288668bed58b80b296368b729cd168be2
David Wright <dave@ritey.com>
2016-11-15 17:46:39 +0000

    Remove ad temporarily

--- resources/views/partials/advert.blade.php
+++ resources/views/partials/advert.blade.php
@@ -1,13 +1,4 @@
 <p class="text-center">Advert</p>
 <div style="max-width:400px">
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- electronic autos -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3396683885146934"
-     data-ad-slot="4795896302"
-     data-ad-format="auto"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+
 </div>
